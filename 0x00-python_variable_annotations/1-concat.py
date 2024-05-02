54279cbbb0ad4e4eec816e4b741e97cc8b1b8839
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-02 22:40:55 +0300

    task1,2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""1-concat.py"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ type-annotated function to return a concatenated string """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""2-floor.py"""
+
+
+def floor(n: float) -> int:
+    """ type-annotated function to return the floor of the float """
+    return int(n)
