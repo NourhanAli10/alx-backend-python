e78241b1ed96143e84f5c76ec1b49075ec8aded7
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-07 17:42:14 +0300

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> AsyncIterator[float]:
     """coroutine called async_generator that takes no arguments."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
