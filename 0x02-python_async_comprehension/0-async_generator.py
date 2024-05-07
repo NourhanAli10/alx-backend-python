2d298c799d8a158644084638e90a3dfc2080e354
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-07 17:39:53 +0300

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""0-async_generator.py"""
+import asyncio
+import random
+from typing import AsyncIterator
+
+
+async def async_generator() -> AsyncIterator[float]:
+    """coroutine called async_generator that takes no arguments."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
